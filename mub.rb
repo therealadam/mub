d3ade28e5a6b254b433239f1221717222887151a
Adam Keys <adam@therealadam.com>
2008-11-26 11:02:59 -0600

    Categorize commit filenames and print totals for each commit.

--- mub.rb
+++ mub.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 #
-# Mechanical Uncle Bob
+# MUB is the Mechanical Uncle Bob
 #
-# Iterates through your source repository and figures out if you're writing
+# It iterates through your source repository and figures out if you're writing
 # enough tests. If you aren't, it yells at you.
 #
 # DISCLAIMER: I have not met Uncle Bob. He may or may not yell at you and he 
@@ -35,14 +35,42 @@ class MechanicalUncleBob
     repo.commit_stats('master', limit)
   end
   
+  def totals_for(diffstats)
+    stats = {:app => [], :tests => []}
+    diffstats.each do |diffstat|
+      next if ignore?(diffstat.filename)
+      stats[category_for(diffstat.filename)] << diffstat.net
+    end
+    
+    {:app => stats[:app].inject(0) { |sum, value| sum + value },
+     :tests => stats[:app].inject(0) { |sum, value| sum + value }}
+  end
+  
+  def category_for(path)
+    key = case path
+    when /^app/ then :app
+    when %r{^public/javascripts} then :app
+    when /^features/ then :tests
+    when /^test/ then :tests
+    else raise "Unknown category for #{path}"
+    end
+  end
+  
+  def ignore?(path)
+    [/^doc/, 
+     %r{^public/images}, 
+     %r{^public/stylesheets}, 
+     /^config/].any? { |pattern| pattern.match(path) }
+  end
+  
   def run
     stats = commits.inject({}) do |stats, (commit_sha, commit_stats)|
       stats[commit_sha] = commit_stats.to_diffstat
-      # stats[commit_sha] = commit_stats.to_hash
       stats
     end
     
-    pp stats
+    totals = stats.map { |(commit, stat)| totals_for(stat) }
+    pp totals
   end
   
 end
